536cc9b9864534e9826c294373022705de4b0527
Alex Marshall <alex.k.marshall83@gmail.com>
2020-05-29 11:21:59 +0100

    Update basic metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Clipboard`,
+    description: `Implementation of a product landing page. From a frontendmentor.io challenge`,
+    author: `@alexkmarshall1`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -31,4 +31,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
