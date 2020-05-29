a7ea730677b2385c67801ae2ef6da4f62e59fb2f
Alex Marshall <alex.k.marshall83@gmail.com>
2020-05-29 12:09:09 +0100

    Change meta image back to jpg
    Facebook just takes a while to scrape the content. It should work

--- src/components/seo.js
+++ src/components/seo.js
@@ -9,7 +9,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Helmet } from "react-helmet";
 import { useStaticQuery, graphql } from "gatsby";
-import defaultMetaImage from "./../images/meta-image.png";
+import defaultMetaImage from "./../images/meta-image.jpg";
 
 function SEO({ description, lang, meta, title }) {
   const { site } = useStaticQuery(
