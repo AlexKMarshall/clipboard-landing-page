1d335d5e399966190493c5965476b1244bcfe665
Alex Marshall <alex.k.marshall83@gmail.com>
2020-05-29 14:40:57 +0100

    Add basic site markup

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,91 @@ import React from "react";
 
 import SEO from "../components/seo";
 
-const IndexPage = () => <SEO title="Landing Page" />;
+const IndexPage = () => (
+  <>
+    <SEO title="Landing Page" />
+    <h1>A history of everything you copy</h1>
+
+    <p>
+      Clipboard allows you to track and organize everything you copy. Instantly
+      access your clipboard on all your devices.
+    </p>
+
+    <button>Download for iOS</button>
+    <button>Download for Mac</button>
+
+    <h2>Keep track of your snippets</h2>
+
+    <p>
+      Clipboard instantly stores any item you copy in the cloud, meaning you can
+      access your snippets immediately on all your devices. Our Mac and iOS apps
+      will help you organize everything.
+    </p>
+
+    <h3>Quick Search</h3>
+    <p>
+      Easily search your snippets by content, category, web address,
+      application, and more.
+    </p>
+
+    <h3>iCloud Sync</h3>
+    <p>Instantly saves and syncs snippets across all your devices.</p>
+
+    <h3>Complete History</h3>
+    <p>
+      Retrieve any snippets from the first moment you started using the app.
+    </p>
+
+    <h2>Access Clipboard anywhere</h2>
+    <p>
+      Whether you’re on the go, or at your computer, you can access all your
+      Clipboard snippets in a few simple clicks.
+    </p>
+
+    <h2>Supercharge your workflow</h2>
+    <p>We’ve got the tools to boost your productivity.</p>
+
+    <h3>Create blacklists</h3>
+    <p>
+      Ensure sensitive information never makes its way to your clipboard by
+      excluding certain sources.
+    </p>
+
+    <h3>Plain text snippets</h3>
+    <p>Remove unwanted formatting from copied text for a consistent look.</p>
+
+    <h3>Sneak preview</h3>
+    <p>Quick preview of all snippets on your Clipboard for easy access.</p>
+
+    <h2>Clipboard for iOS and Mac OS</h2>
+    <p>
+      Available for free on the App Store. Download for Mac or iOS, sync with
+      iCloud and you’re ready to start adding to your clipboard.
+    </p>
+
+    <button>Download for iOS</button>
+    <button>Download for Mac</button>
+
+    <ul>
+      <li>FAQs</li>
+      <li>Contact Us</li>
+      <li>Privacy Policy</li>
+      <li>Press Kit</li>
+      <li>Install Guide</li>
+    </ul>
+
+    <p class="attribution">
+      Challenge by{" "}
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by <a href="https://github.com/AlexKMarshall">Alex Marshall</a>.
+    </p>
+  </>
+);
 
 export default IndexPage;
