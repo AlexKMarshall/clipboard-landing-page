755acd3a220b88537f487a78bdae1f5f0d4bcd29
Alex Marshall <alex.k.marshall83@gmail.com>
2020-06-04 18:35:37 +0100

    Style basic typography

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,9 @@ import SEO from "../components/seo";
 const IndexPage = () => (
   <>
     <SEO title="Landing Page" />
-    <h1>A history of everything you copy</h1>
+    <h1 className="text-4xl font-bold tracking-tight">
+      A history of everything you copy
+    </h1>
 
     <p>
       Clipboard allows you to track and organize everything you copy. Instantly
@@ -15,7 +17,9 @@ const IndexPage = () => (
     <button>Download for iOS</button>
     <button>Download for Mac</button>
 
-    <h2>Keep track of your snippets</h2>
+    <h2 className="text-3xl font-bold tracking-tight">
+      Keep track of your snippets
+    </h2>
 
     <p>
       Clipboard instantly stores any item you copy in the cloud, meaning you can
@@ -23,42 +27,54 @@ const IndexPage = () => (
       will help you organize everything.
     </p>
 
-    <h3>Quick Search</h3>
-    <p>
+    <h3 className="text-2xl font-bold tracking-tight">Quick Search</h3>
+    <p className="text-sm">
       Easily search your snippets by content, category, web address,
       application, and more.
     </p>
 
-    <h3>iCloud Sync</h3>
-    <p>Instantly saves and syncs snippets across all your devices.</p>
+    <h3 className="text-2xl font-bold tracking-tight">iCloud Sync</h3>
+    <p className="text-sm">
+      Instantly saves and syncs snippets across all your devices.
+    </p>
 
-    <h3>Complete History</h3>
-    <p>
+    <h3 className="text-2xl font-bold tracking-tight">Complete History</h3>
+    <p className="text-sm">
       Retrieve any snippets from the first moment you started using the app.
     </p>
 
-    <h2>Access Clipboard anywhere</h2>
+    <h2 className="text-3xl font-bold tracking-tight">
+      Access Clipboard anywhere
+    </h2>
     <p>
       Whether you’re on the go, or at your computer, you can access all your
       Clipboard snippets in a few simple clicks.
     </p>
 
-    <h2>Supercharge your workflow</h2>
+    <h2 className="text-3xl font-bold tracking-tight">
+      Supercharge your workflow
+    </h2>
     <p>We’ve got the tools to boost your productivity.</p>
 
-    <h3>Create blacklists</h3>
-    <p>
+    <h3 className="text-2xl font-bold tracking-tight">Create denylists</h3>
+    <p className="text-sm">
       Ensure sensitive information never makes its way to your clipboard by
       excluding certain sources.
     </p>
 
-    <h3>Plain text snippets</h3>
-    <p>Remove unwanted formatting from copied text for a consistent look.</p>
+    <h3 className="text-2xl font-bold tracking-tight">Plain text snippets</h3>
+    <p className="text-sm">
+      Remove unwanted formatting from copied text for a consistent look.
+    </p>
 
-    <h3>Sneak preview</h3>
-    <p>Quick preview of all snippets on your Clipboard for easy access.</p>
+    <h3 className="text-2xl font-bold tracking-tight">Sneak preview</h3>
+    <p className="text-sm">
+      Quick preview of all snippets on your Clipboard for easy access.
+    </p>
 
-    <h2>Clipboard for iOS and Mac OS</h2>
+    <h2 className="text-3xl font-bold tracking-tight">
+      Clipboard for iOS and Mac OS
+    </h2>
     <p>
       Available for free on the App Store. Download for Mac or iOS, sync with
       iCloud and you’re ready to start adding to your clipboard.
